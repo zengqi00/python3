8b75c541b0a512629ba14b3eb3558208e81a4896
曾琪 <zengqi@yeechoo.com>
2019-12-28 12:51:17 +0800

    432432432432

---
+++ djangodemo/xiaoshuo/novel/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('hello world')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
